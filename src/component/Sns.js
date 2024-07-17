1f2c766d1e7dcbb0adff3201b82c29942e726cd1
srh0820 <srh96820@gmail.com>
2024-07-17 12:02:52 +0900

    sns리뷰 컴포넌트 분리

--- src/component/Sns.js
+++ src/component/Sns.js
@@ -1,31 +1,9 @@
-import React, { useState } from 'react';
-import instagram from '../img/instagram1.svg';
+import React from 'react'
 import snsscss from './scss/srh.module.scss'; 
+import instagram from '../img/instagram1.svg';
+import SnsImg from './sns/SnsImg';
 
 function Sns() {
-    const imgIndices = [1, 2, 3, 4, 5, 6, 7, 8]; // 이미지 인덱
-
-    const [hoveredIndex, setHoveredIndex] = useState(null);
-    // hoveredIndex는 현재 호버된 이미지의 인덱스를 저장하는 상태 변수
-    // setHoveredIndex는 hoveredIndex 상태를 업데이트하는 함수
-    // 초기값은 null로 설정하여 아무 이미지도 호버되지 않은 상태
-   
-    const handleMouseEnter = (index) => {
-        setHoveredIndex(index);
-    };
-    // handleMouseEnter 함수는 이미지에 마우스가 올라갔을 때 호출
-    // index 매개변수는 호버된 이미지의 인덱스
-    // setHoveredIndex(index)를 호출하여 hoveredIndex 상태를 호버된 이미지의 인덱스로 업데이트
-
-    const handleMouseLeave = () => {
-        setHoveredIndex(null);
-    };
-    // handleMouseLeave 함수는 이미지에서 마우스가 떠났을 때 호출
-    // setHoveredIndex(null)를 호출하여 hoveredIndex 상태를 null로 업데이트. 이는 어떤 이미지도 호버되지 않은 상태
-
-    // 사용자가 이미지에 마우스를 올리면 handleMouseEnter 함수가 호출되어 hoveredIndex가 해당 이미지의 인덱스로 설정
-    // 사용자가 이미지에서 마우스를 떼면 handleMouseLeave 함수가 호출되어 hoveredIndex가 null로 설정
-
     return (
         <section className={snsscss.sns}>
             <div className="container-1400">
@@ -56,20 +34,8 @@ function Sns() {
                     </div>
                 </div>
                 
-                <div className="row d-flex align-items-stretch"> 
-                    {imgIndices.map(index => (
-                        <div 
-                            key={index} 
-                            className="col-lg-3 col-md-6 mb-4"
-                            onMouseEnter={() => handleMouseEnter(index)}
-                            onMouseLeave={handleMouseLeave}
-                        >
-                            <div className={`${snsscss.snsImgR} d-flex justify-content-center align-items-center ${hoveredIndex !== null && hoveredIndex !== index ? snsscss.blurred : ''}`}>
-                                <img src={`/img/sns${index}.jpg`} alt={`인스타그램 이미지 ${index}번`} className="img-fluid" />
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <SnsImg></SnsImg>
+
             </div>
         </section>
     );
