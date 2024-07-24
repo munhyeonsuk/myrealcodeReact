f06a2ad042f902e5cb280f7f968849b058820710
thgml21004 <thgml21004@gmail.com>
2024-07-24 17:22:09 +0900

    문의사항접수 퀵버튼 click

--- src/component/Popupform.js
+++ src/component/Popupform.js
@@ -1,30 +1,30 @@
 import popupscss from './scss/psh.module.scss'
 import Close from '../img/formClose.svg'
 
-function Popupform() {
+function Popupform({onOpen, onClose}) {
     return (
-        <div className={popupscss.popupform}>
+        <div className={popupscss.popupform} style={{display: onOpen ? "block" : "none"}}>
             <div className={popupscss.popupWrapS}>
                 <div className={`${popupscss.popupHeaderS} d-flex justify-content-between`}>
                     <h3 className="mb-0">문의 사항 접수</h3>
-                    <button className={popupscss.close} id="close">
+                    <button onClick={onClose} className={popupscss.close} id="close">
                         <img src={Close} alt="닫기" />
                     </button>
                 </div>
                 <div className={popupscss.popupBodyS}>
                     <ul className="ps-0">
-                    <li className={popupscss.point}>※ 문의 전 안내사항을 꼭 읽어주세요.</li>
-                    <li>답변은 회원가입 시 등록한 이메일 주소로 전달드립니다.</li>
-                    <li>접수 후 영업일 기준 2~3일 후 답변이 전달 받을 수 있습니다.</li>
-                    <li>사람이 직접 답변을 작성하기 때문에 문의가 많을 시 답변이 오래걸릴 수 있습니다.</li>
+                        <li className={popupscss.point}>※ 문의 전 안내사항을 꼭 읽어주세요.</li>
+                        <li>답변은 회원가입 시 등록한 이메일 주소로 전달드립니다.</li>
+                        <li>접수 후 영업일 기준 2~3일 후 답변이 전달 받을 수 있습니다.</li>
+                        <li>사람이 직접 답변을 작성하기 때문에 문의가 많을 시 답변이 오래걸릴 수 있습니다.</li>
                     </ul>
                     <form action="">
                     <div>
                         <label for="type">문의 유형</label>
                         <select id="type">
-                        <option value="" disabled selected>문의 유형을 선택해주세요.</option>
-                        <option value="">예약 문의</option>
-                        <option value="">이벤트 문의</option>
+                            <option value="" disabled selected>문의 유형을 선택해주세요.</option>
+                            <option value="">예약 문의</option>
+                            <option value="">이벤트 문의</option>
                         </select>
                     </div>
                     <div>
@@ -38,7 +38,7 @@ function Popupform() {
                     </form>
                 </div>
                 <div className={`${popupscss.popupFooterS} d-flex justify-content-between`}>
-                    <button>취소</button>
+                    <button onClick={onClose}>취소</button>
                     <button className={popupscss.confirm}>확인</button>
                 </div>
             </div>
--- src/component/Quick.js
+++ src/component/Quick.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import up from '../img/quickUp.svg'
 import form from '../img/quickQuestion.svg'
 import ai from '../img/quickAI.svg'
 import quscss from './scss/mhs.module.scss'
+
+import Popupform from './Popupform'
+
 const Quick = () => {
-    return (
+  const [popVisible, setPopVisible] = useState(false);
+
+  const popOpen = () => {
+    setPopVisible(true);
+  }
+
+  const popClose = () => {
+    setPopVisible(false);
+  }
+
+  return (
+    <>
       <section className={quscss.quick}>
         <ul>
           <li><img src={up} alt="" />
             <span className="visually-hidden">위로</span>
           </li>
-          <li className="questionIcon"><img src={form} alt="" /></li>
+          <li className="questionIcon" onClick={popOpen}><img src={form} alt="" /></li>
           <li className="position-relative"><img src={ai} alt="" />
             <span className="mt-2 position-absolute">AI TRIP PLANNER</span>
           </li>
-        </ul>
+        </ul>      
       </section>
-    );
-  };
+      <Popupform onOpen={popVisible} onClose={popClose} />
+    </>    
+  );
+};
   
-  export default Quick;
+export default Quick;
