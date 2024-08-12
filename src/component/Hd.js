5716875f7d651b5417b14b55cbc522ea064bc3ed
thgml21004 <thgml21004@gmail.com>
2024-08-12 17:47:15 +0900

    [1.1.11] 헤더 1024 디자인 두가지 버전

--- src/component/Hd.js
+++ src/component/Hd.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import hdscss from './scss/mhs.module.scss'
 import './scss/mhs.hd.scss'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components';
 
 import logo from '../img/hdLogo.svg'
 import search from '../img/hdSearch.svg'
@@ -13,7 +14,44 @@ import Hdnavi from './hd/Hdnavi';
 import Hdpopular from './hd/Hdpopular';
 import Mnavi from './hd/Mnavi';
 
+const HdWrap = styled.header`
+    &.showMnavi {
+        background: transparent !important;
+        padding: 0;
+        &:after {
+            content: "";
+            display:block;
+            width: 100vw;
+            height: 100vh;
+            background: rgba(0,0,0,0.5);
+            position: fixed;
+            top: 0;
+    
+        }
+        & > div {
+            max-width: 490px;
+            width: 100%;
+            background-color: #FFEBB7;
+            position: relative;
+            z-index: 1;
+            padding: 13px 15px;
+            h1 {
+                margin : 0 auto;
+            }
+        }
+    }
+`;
+
+
+const HdBtn = styled.button`
+    position: fixed;
+    right:0;
+    bottom:0;
+`;
+
 function Hd() {
+    const [hd, setHd] = useState(true);
+
     const [bgColor, setBgColor] = useState("transparent"); // 헤더 배경색 초기값
     const [SubClass, setSubClass] = useState(null); // 헤더 border-bottom 초기값
     const [showMnavi, setShowMnavi] = useState(false); // Mnavi 표시 여부
@@ -113,7 +151,8 @@ function Hd() {
 
     return (
         <>
-            <header className={`${hdscss.hd} fixed-top ${showMnavi ? "showMnavi" : "" }`} style={{ backgroundColor: bgColor }}>
+            { hd ? <header className={`${hdscss.hd} fixed-top ${showMnavi ? "showMnavi" : "" }`} style={{ backgroundColor: bgColor }}>
+                <HdBtn onClick={ () => { setHd(false)}}>헤더2</HdBtn>
                 <div className="container-1824 d-flex justify-content-between align-items-center">
                     {showAllMenuBtn && (
                         <button className={`${hdscss.allMenuBtn} p-0`} onClick={handleAllMenuBtnClick}>
@@ -163,6 +202,59 @@ function Hd() {
                 </div>
                 {showMnavi && <Mnavi handleCloseBtnClick={handleCloseBtnClick} />}
             </header>
+            : <HdWrap className={`${hdscss.hd} fixed-top ${showMnavi ? "showMnavi" : "" }`} style={{ backgroundColor: bgColor }}>
+                <HdBtn onClick={ () => { setHd(true)}}>헤더</HdBtn>
+            <div>
+                <div className="container-1824 d-flex justify-content-between align-items-center">
+                    {showAllMenuBtn && (
+                        <button className={`${hdscss.allMenuBtn} p-0`} onClick={handleAllMenuBtnClick}>
+                            <img src={allbtn} alt="메뉴" />
+                        </button>
+                    )}
+                    <h1><a href="/"><img src={logo} alt="" /></a></h1>
+                    <ul className={`${hdscss.gnb} ${SubClass} d-flex ps-0 mb-0`}>
+                        <Hdnavi handleCloseBtnClick={handleCloseBtnClick} /> {/* handleCloseBtnClick 전달 */}
+                    </ul>
+                    <ul className={`${hdscss.utill}  justify-content-end position-relative mb-0 p-0 ${showMnavi ? "d-none" : "d-flex" }`}>
+                        <li className="position-relative">
+                            {showSearchIcon ? (
+                                <img src={search} alt="검색" className={`${hdscss.searchimg} open`} />
+                            ) : (
+                                <img src={close} alt="닫기" className={`${hdscss.closeimg}`} onClick={handleCloseBtnClick} />
+                            )}
+                            <span className="visually-hidden">검색</span>
+
+                            <div className={`${hdscss.searchMenu} searchdivbox d-none`}>
+                                <div className={`${hdscss.dropdownbox} m-0`}>
+                                    <div className={`${hdscss.searchbar} d-flex justify-content-between align-items-center text-end pb-1`}>
+                                        <img className={`${hdscss.searchimg} pe-2`} src={search} alt="검색" />
+                                        <input type="text" placeholder='검색' className={hdscss.searchinput} />
+                                        <button className={`${hdscss.searchbt} pe-1 close`} ><img src={close} alt="닫기" className={`${hdscss.closeImg}`} /></button>
+                                    </div>
+                                    <div className={`${hdscss.popularbar} p-0`}>
+                                        <p className={`${hdscss.searchPopular} pt-2 mb-1 text-black-50`}>실시간인기</p>
+                                        <div className={`${hdscss.searchTop10} d-flex`}>
+                                            <div>
+                                                <Hdpopular />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </li>
+                        <li className={`${hdscss.loginImg} ms-4 `}>
+                            <Link><img src={login} alt="로그인" /></Link>
+                            <span className="visually-hidden">로그인</span>
+                        </li>
+                    </ul>
+                    {!isDesktop && showMnavi && (
+                        <button className={hdscss.mnaviclose} onClick={handleCloseBtnClick}><img src={close} alt="닫기" /></button>
+                    )}
+                </div>
+                {showMnavi && <Mnavi handleCloseBtnClick={handleCloseBtnClick} />}                
+            </div>
+        </HdWrap>}
         </>
     );
 }
